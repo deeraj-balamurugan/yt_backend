f29cd6301e9aaefa01b784d44821dbe2caedc99c
Deeraj <deerajbalamurugan@gmail.com>
2024-03-27 23:29:44 +0530

    added files to utils folder for asyncHandler, ApiResponse, ApiError

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+const uri = process.env.MONGODB_URI;
+
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const connectionInstance = await mongoose.connect(`${uri}/${DB_NAME}`);
     console.log(
-      `\n MongoDB Connected - db host: ${connectionInstance.connection.host}`
+      `mongoDB connected - db host: ${connectionInstance.connection.host}`
     );
-  } catch (error) {
-    console.log("MongoDB connection failed", error);
+  } catch (err) {
+    console.log("mongoDB connection failed", err);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("----------error---------", err);
+      throw err;
+    });
+    app.listen(port || 8000, () => {
+      console.log(`server is running on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection failed", err);
+  });
